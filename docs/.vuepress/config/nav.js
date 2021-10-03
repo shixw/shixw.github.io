0acd6a22722a006bbb93a979f2b027d2a4ade00c
shixianwei <shixw_usr@126.com>
2021-10-03 23:00:07 +0800

    Mac使用技巧

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -21,22 +21,16 @@ module.exports = [
     ],
   },
   {
-    text: '框架使用',
+    text: '开源框架',
     link: '/framework/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
       {
-        text: '日志框架',
-        items: [
-          { text: 'LOG4J', link: '/pages/e65eef/' },
-        ],
+        text: '日志框架',link: '/pages/6bd25c/'
       },
-      // {
-      //   text: '学习笔记',
-      //   items: [
-      //     { text: '《JavaScript教程》', link: '/note/javascript/' },
-      //   ],
-      // },
+      {
+        text: '全链路跟踪', link: '/apm/'
+      },
     ],
   },
   {
@@ -54,18 +48,14 @@ module.exports = [
     items: [
       // { text: '技术框架', link: '/pages/eee83a9211a70f9d/' },
       // { text: '网站', link: '/pages/eee83a9211a70f9d/' },
-      { text: '资源', link: '/pages/eee83a9211a70f9d/' },
+      { text: '资源', link: '/pages/beb6c0bd8a66cea6/' },
     ],
   },
   {
     text: '更多',
     link: '/more/',
     items: [
-      { text: '学习', link: '/pages/f2a556/' },
-      { text: '面试', link: '/pages/aea6571b7a8bae86/' },
-      { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
-      { text: '实用技巧', link: '/pages/baaa02/' },
-      { text: '友情链接', link: '/friends/' },
+      { text: 'MAC使用技巧', link: '/macos/' },
     ],
   },
   // {
