ad15aaee9ada896585bd53fa52b3e8ccf7ebf5c3
shixianwei2 <shixianwei@jd.com>
2021-10-08 15:02:58 +0800

    Spring先关内容

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -21,26 +21,28 @@ module.exports = [
     ],
   },
   {
-    text: '技术',
-    link: '/technology/',
-    items: [
-      { text: 'MySQL', link: '/mysql/' },
-    ],
-  },
-  {
-    text: '开源框架',
+    text: '框架',
     link: '/framework/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
       {
+        text: 'Spring',link: '/spring/'
+      },
+      {
         text: '日志框架',link: '/pages/6bd25c/'
       },
+    ],
+  },
+  {
+    text: '技术',
+    link: '/technology/',
+    items: [
+      { text: 'MySQL', link: '/mysql/' },
       {
         text: '全链路跟踪', link: '/apm/'
       },
     ],
   },
-
   // { text: '关于', link: '/about/' },
   {
     text: '收藏',
