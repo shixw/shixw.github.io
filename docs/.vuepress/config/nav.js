039005ee8d7a4138b27b8e066b374a6507b8e3d1
shixianwei2 <shixianwei@jd.com>
2022-11-23 10:39:33 +0800

    Golang

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -21,6 +21,12 @@ module.exports = [
     ],
   },
   {
+    text: 'Golang',
+    link: '/golang/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+    ],
+  },
+  {
     text: '框架',
     link: '/framework/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
